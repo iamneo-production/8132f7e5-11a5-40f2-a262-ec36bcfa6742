95d721883acc224d1d7dec68685932f3462968a1
Suhrrid2000 <suhrrid123@gmail.com>
2021-09-29 11:04:19 +0000

    Login Page Done

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:LoginComponent,
+    pathMatch:'full'
+  },
+  {
+    path:"login",
+    component:LoginComponent,
+    pathMatch:'full'
+  },
+  {
+    path:"signup",
+    component:SignupComponent,
+    pathMatch:'full'
+  },
+  {
+    path:"home",
+    component:HomeComponent,
+    pathMatch:'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,14 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ModelComponent } from './model/model.component';
+import { ServicesComponent } from './services/services.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserOrderComponent } from './user-order/user-order.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarLoginComponent } from './navbar-login/navbar-login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HomeComponent,
+    LoginComponent,
+    ModelComponent,
+    ServicesComponent,
+    SignupComponent,
+    UserOrderComponent,
+    AdminComponent,
+    NavbarLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
